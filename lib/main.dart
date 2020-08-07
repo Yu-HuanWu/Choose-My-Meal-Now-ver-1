fccb61d404fe4fe7ca528386962dc6c50316dafd
Yu-HuanWu <69253764+Yu-HuanWu@users.noreply.github.com>
2020-08-07 01:08:59 -0700

    Moving background
    Finally, the background is moving.
    
    Co-Authored-By: Aaron Hong <gnoha@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -6,17 +8,23 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        title: Text('Choose My Meal Now!'),
-        backgroundColor: Colors.blueGrey[200],
+        title: Text('Choose My Meal Now!',
+        textAlign: TextAlign.center,
+        style: TextStyle(color: Colors.amberAccent, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.lightGreen,
       ),
       body:
       Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text('Ready to find some good grubs?',
-            style: TextStyle(fontSize: 40)),
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 40,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic)),
             RaisedButton(
                 onPressed: () {
                   Navigator.push(
@@ -30,9 +38,7 @@ class FirstRoute extends StatelessWidget {
                   style: TextStyle(fontSize: 20),
                 )
             ),
-            Image(image:
-            AssetImage('images/mealimage.png'))
-          ]
+             ]
       ),
     );
   }
@@ -54,10 +60,76 @@ appBar: AppBar(
   }
 }
 
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) :
+      super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() =>
+      _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse:true);
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(-0.1,0.0),
+      end: const Offset(0.1, 0.0),
+    ).animate(
+        CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutSine,
+    ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Image(image:
+        AssetImage('images/mealimage.png'),
+      height: double.maxFinite,
+        repeat: ImageRepeat.repeat,
+      ),
+    );
+  }
+}
 
 void main() => runApp(
       MaterialApp(
-        home: FirstRoute()
+        home: Stack(
+        children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.amberAccent,
+
+            /*image: new DecorationImage(image:
+            new AssetImage('images/mealimage.png'),
+            fit: BoxFit.cover),*/
+          ),
+        child: MyStatefulWidget(),
+        //FirstRoute()
     ),
+  Container(
+    child: FirstRoute(),
+      ),
+],
+),
+      ),
 );
 
